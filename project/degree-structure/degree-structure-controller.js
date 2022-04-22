9dc00f607f79199974f5b44b0c2d5080a1b4b267
ruwen <ruwen.yi@hotmail.com>
2022-04-22 16:10:58 +0930

    set route

--- project/degree-structure/degree-structure-controller.js
+++ project/degree-structure/degree-structure-controller.js
@@ -1,13 +1,20 @@
 /* Import DAO object which contains data access function */
 const { DegreeStructureDAO } = require("./degree-structure-dao");
 
-
-/* Get core courses from the database and then render them */
+/* Get core/elec courses from the database and then render them */
 const renderDegreeStructure = (req, res) => {
-    let degreeStructure = {};
-    degreeStructure.core = DegreeStructureDAO.getCoreCourses(req.params.degreeID);
-    degreeStructure.elec = DegreeStructureDAO.getElectoveCourses(req.params.degreeID);
+    let degreeID = req.params.degreeID;
 
+    // We only have 6 degrees, any other attempts are invalid
+    if (degreeID < 0 || degreeID > 6)
+        return res.status(200).send(`Not exists`);
+
+    // Get core/elec courses by using functions in DAO
+    let degreeStructure = {};
+    degreeStructure.core = DegreeStructureDAO.getCoreCourses(degreeID);
+    degreeStructure.elec = DegreeStructureDAO.getElectoveCourses(degreeID);
+
+    // Render data, according to your UI desing
     res.status(200).send(`${degreeStructure.core} <br> ${degreeStructure.elec} <br> Render!!`);
 }
 
