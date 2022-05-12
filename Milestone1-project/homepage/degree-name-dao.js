1c43c7322e5d5b0ff63d8645a4335ed3aac8f286
ruwen <reuwenyi@gmail.com>
2022-05-12 14:33:37 +0930

    docs:rename

--- Milestone1-project/homepage/degree-name-dao.js
+++
@@ -1,8 +0,0 @@
-/* Access/Process Data from database */
-
-/* Return all degree names from the database */
-function getAllDegreeName() {
-    return '*DEGREE NAME*';
-}
-
-module.exports = getAllDegreeName;
