87019a116c9bc08eab5cc575d62fcee9d8422da4
ruwen <reuwenyi@gmail.com>
2022-05-19 12:41:38 +0930

    use mysql2 and async package to do loop query

--- project/homepage/test.js
+++ project/homepage/test.js
@@ -3,71 +3,117 @@ const mysqlConnection = require('../app');
 
 const async = require('async')
 
-/* Access data and render degree names*/
-const test_course = (req, res) => {
-    let qq = 'SELECT * FROM adelaide.course;';
-    mysqlConnection.query(qq, async(err, info, fields) => {
-    if (!err){
-        var data =JSON.stringify(info);
-        data = JSON.parse(data);
-
-        tem = setData1(data);
-        
-        console.log(tem[0])
-
-        /* Use async package to do for loop query */
-        async.forEachOf(tem, function(tem_item, key, callback){
-            
-            tem[key].belongs_to = "test work!"
-
-            //let q_for_each = ``;
-            // mysqlConnection.query(q_for_each, (err, info, fields) => {
-            //     if (!err){
-            //         var test_data =JSON.stringify(info);
-            //         test_data = JSON.parse(test_data);
-            //         tem[key].belongs_to = test_data;
-            //     }
-            //     else{
-            //         console.log(err);
-            //     }
-            //     });
-
-            } , function(err){
-                if(err) {console.error(err.message);}
-        })
-        res.send(tem)
-        //res.render('../views/test.ejs', {arr:tem})
-    }
-    else{
-        console.log(err);
-    }
+async function main(req, res) {
+    const mysql2 = require('mysql2/promise');
+    const connection = await mysql2.createConnection({
+    host: 'database-2.cwlp4i7l59rt.ap-southeast-2.rds.amazonaws.com',
+    user: 'admin',
+    password: '12345678', //请与上一步在数据库设置的密码相同
+    database: 'adelaide'
     });
-};
+    var list = await connection.execute('SELECT * FROM adelaide.course');
+
+    // list = [ [ {}, {}, ... ] ] so we need to get the inner array: [ {}, {}, ... ]
+    list = list[0]
+
+    // only use the first three obj as an example
+    list = list.slice(0,3)
+
+    // console.log(list)
+
+    var result = []
+
+    async.forEachOf(list, async function(item, key, callback){
+        item = await connection.execute('SELECT * FROM adelaide.course');
+        
+        // item = [ [ {}, {}, ... ] ] so we need to get the inner array: [ {}, {}, ... ]
+        // only use the first three obj as an example
+        item = item[0].slice(0,3)
+        
+        //console.log(item)
+        
+        result.push(item);
+    }, 
+    function(err){
+        if(err) {console.error(err.message);}
+
+        // result = [ item 1: [ {}, {}, ... ] item 2: [ {}, {}, ... ] item 3: [ {}, {}, ... ] ] as expected !!
+        res.send(result)
+    })
+    
+    
+}
 
 
-function setData1(data11){
-    let courses=[];
-    for(i=0;i<data11.length;i++){
-        let course = {
-            course_subject_code: "COMP SCI 0000",
-            course_name: "Computer course name",
-            course_url:"http://course-outline/.....",
-            belongs_to: [
-            ],
-            pre_requisite: "One of COMP SCI 7777, COMPSCI 7777, ...",
-            incompatible: "COMP SCI 7777, COMPSCI 7777, and ...",
-        };
-        course.course_subject_code = data11[i].course_code;
-        course.course_name =  data11[i].course_name;
-        course.course_url =  data11[i].courselink_href;
-        course.pre_requisite =  data11[i].pre_requisite;
-        course.incompatible = data11[i].Incompatibale;
-        courses.push(course);
 
-        //console.log(course)
-    }
-    return courses;
-};
+/* Access data and render degree names*/
+// const test_course = (req, res) => {
+//     let qq = 'SELECT * FROM adelaide.course;';
+
+//     mysqlConnection.query(qq, async(err, info, fields) => {
+//     if (!err){
+//         var data =JSON.stringify(info);
+//         data = JSON.parse(data);
+
+//         tem = setData1(data);
+        
+//         console.log(tem[0])
+
+//         tem = tem.slice(0,3);
+
+//         /* Use async package to do for loop query */
+//         async.forEachOf(tem, async function(tem_item, key, callback){
+            
+//             //tem[key].belongs_to = "test work!"
+
+//             let q_for_each = 'SELECT * FROM adelaide.course;';
+//             mysqlConnection.query(q_for_each, (err, info, fields) => {
+//                 if (!err){
+//                     var test_data =JSON.stringify(info);
+//                     test_data = JSON.parse(test_data);
+//                     tem[key].belongs_to = test_data;
+//                 }
+//                 else{
+//                     console.log(err);
+//                 }
+//             });
+
+//             } , function(err){
+//                 if(err) {console.error(err.message);}
+//         })
+//         res.send(tem)
+//         //res.render('../views/test.ejs', {arr:tem})
+//     }
+//     else{
+//         console.log(err);
+//     }
+//     });
+// };
+
+
+// function setData1(data11){
+//     let courses=[];
+//     for(i=0;i<data11.length;i++){
+//         let course = {
+//             course_subject_code: "COMP SCI 0000",
+//             course_name: "Computer course name",
+//             course_url:"http://course-outline/.....",
+//             belongs_to: [
+//             ],
+//             pre_requisite: "One of COMP SCI 7777, COMPSCI 7777, ...",
+//             incompatible: "COMP SCI 7777, COMPSCI 7777, and ...",
+//         };
+//         course.course_subject_code = data11[i].course_code;
+//         course.course_name =  data11[i].course_name;
+//         course.course_url =  data11[i].courselink_href;
+//         course.pre_requisite =  data11[i].pre_requisite;
+//         course.incompatible = data11[i].Incompatibale;
+//         courses.push(course);
+
+//         //console.log(course)
+//     }
+//     return courses;
+// };
 
 /* Export the function to be used by routes.js */
-module.exports = test_course;
+module.exports = main;
