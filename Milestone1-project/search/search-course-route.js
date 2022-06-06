8c15e7b8cda6bc95d1812aad32b9cd0ac674f14a
ruwen <ruwen.yi@adelaide.edu.au>
2022-06-06 17:35:15 +0930

    manage folder

--- Milestone1-project/search/search-course-route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const searchCourse = require('./search-course-controller');
-
-router.route('').get(searchCourse);
-
-module.exports = router;
