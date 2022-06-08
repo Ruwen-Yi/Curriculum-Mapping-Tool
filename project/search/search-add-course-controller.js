5f869804d3820f6c6c58dbfd835ec6a600731464
yuhao <791633900@qq.com>
2022-06-08 21:02:35 +0930

    search add course

--- project/search/search-add-course-controller.js
+++ project/search/search-add-course-controller.js
@@ -3,26 +3,36 @@ const mysqlConnection = require('../app');
 
 const search_add_courses = (req, res)=> {
     
-    // let course_name = req.query['course'].split('-').join(' ');
-    // console.log(course_name);
-    // let qq= '';
-    // mysqlConnection.query(qq, (err, info, fields) => {
-    //     if (!err){
-    //         var data =JSON.stringify(info);
-    //         data = JSON.parse(data);
+    let course_name = req.query['course'].split('-').join(' ');
+    console.log(course_name);
+    let qq= "SELECT * FROM adelaide.course WHERE fullname LIKE '%"+course_name+"%';";
+    mysqlConnection.query(qq, (err, info, fields) => {
+        if (!err){
+            var data =JSON.stringify(info);
+            data = JSON.parse(data);
             
-    //         return process_course_list(req,res,data);
-    //     }
-    //     else{
-    //         console.log(err);
-    //     }
-    // })
+            return process_course_list(req,res,data);
+        }
+        else{
+            console.log(err);
+        }
+    })
 }
 
 /* This function process the raw data and produce a course_list */
 function process_course_list(req,res,data) {
-    let course_list = [{},{}];
+    let course_list = [];
     /* check data stucture, output a course_list */
+    for (i=0;i<data.length;i++){
+        course={
+            course_subject_code: "COMP SCI 0000",
+            course_name: "Computer course name",
+        }
+        course.course_subject_code = data[i].course_code;
+        course.course_name = data[i].course_name;
+        course_list.push(course);
+    }
+    console.log(course_list)
     res.send(course_list);
 }
 
