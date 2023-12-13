7251f529fdeabd1bf836ed009fe30ef60a12ef80
ruwen <ruwen.yi@hotmail.com>
2023-12-13 10:37:00 +0800

    fix(setData function): restore setData funtion

--- homepage/course-overview-controller.js
+++ homepage/course-overview-controller.js
@@ -61,5 +61,29 @@ async function main(req, res) {
     res.render("../views/homepage-course-overview.ejs", { course_list: list });
 }
 
+function setData1(data11){
+    let courses=[];
+    for(i=0;i<data11.length;i++){
+        let course = {
+            course_subject_code: "COMP SCI 0000",
+            course_name: "Computer course name",
+            course_url:"http://course-outline/.....",
+            belongs_to: [],
+            pre_requisite: "One of COMP SCI 7777, COMPSCI 7777, ...",
+            incompatible: "COMP SCI 7777, COMPSCI 7777, and ...",
+        };
+        course.course_subject_code = data11[i].course_code;
+        course.course_name =  data11[i].course_name;
+        course.course_url =  data11[i].courselink_href;
+        course.pre_requisite =  data11[i].pre_requisite;
+        course.incompatible = data11[i].Incompatibale;
+        course.belongs_to = data11[i].belongs_to;
+        courses.push(course);
+
+        //console.log(course)
+    }
+    return courses;
+};
+
 /* Export the function to be used by routes.js */
 module.exports = main;
