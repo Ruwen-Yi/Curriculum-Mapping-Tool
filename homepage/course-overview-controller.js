3447f778c5d91d168cf8404aca30825446a80d23
ruwen <ruwen.yi@hotmail.com>
2023-12-20 12:44:48 +0800

    refactor(asyncDBconnection): remove mysql2/promise, use native promise to achieve synchronous query

--- homepage/course-overview-controller.js
+++ homepage/course-overview-controller.js
@@ -1,64 +1,107 @@
 /* Access database*/
+// const asyncConnection = require('../db/asyncConnection.js');
+const mysqlConnection = require('../app');
+
+const connection = mysqlConnection;
+
 async function main(req, res) {
+    let list = []; 
+    new Promise((resolve, reject) => {
+        connection.execute(
+            "SELECT * FROM `course`",
+            null,
+            (err, courses) => (err ? reject(err) : resolve(courses.slice(0,5)))
+        );
+    })
+    .then(courses => {
+        console.log(courses);
+        // add degrees info to every course
+        const promises = courses.map(course => fetchDegreesByCourse(course));
+        return Promise.all(promises);
+    })
+    .then((results) => {
+        console.log(JSON.stringify(results, null, 2));
+        let list = setData1(results);
+        res.render("../views/homepage-course-overview.ejs", { course_list: list });
+    })
+    .catch(err => {
+        console.error(err.stack);
+    })
 
-    const mysql2 = require('mysql2/promise');
+    // for (i = 0; i < list.length; i++) {
 
-    const connection = await mysql2.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT || 3306,
-        database: process.env.DB_DATABASE,
-        connectTimeout: 60000
-    });
+    //     const query = 'SELECT degree,stream,supplement FROM `degree_course` WHERE `courses` = ?';
+    //     [b_to] = await connection.execute(query, [list[i].fullname]);
+    //     let bt = [];
+    //     let info = {
+    //         degree: "",
+    //         courses: "",
+    //         stream: "",
+    //         supplement: "",
+    //         full: ""
+    //     }
+    //     for (ii = 0; ii < b_to.length; ii++) {
+    //         var reg = RegExp(/Master/)
+    //         var part1, part2, part3;
 
-    // list = [ {}, {}, ... ]
-    let [list] = await connection.execute('SELECT * FROM course');
-    // list = list.slice(0,50)
+    //         if (b_to[ii].degree.match(reg)) {
+    //             part1 = b_to[ii].degree.replace("Master of ", "M-");
+    //         } else if (b_to[ii].degree == "") {
+    //             part1 = ""
+    //         } else {
+    //             part1 = b_to[ii].degree.replace("Bachelor of ", "B-");
+    //         }
 
-    for (i = 0; i < list.length; i++) {
+    //         if (b_to[ii].stream == "") {
+    //             part2 = "";
+    //         } else {
+    //             part2 = "-" + b_to[ii].stream;
+    //         }
 
-        [b_to] = await connection.execute('SELECT degree,stream,supplement FROM `degree_course` WHERE `courses` = "' + list[i].fullname + '";');
-        let bt = [];
-        let info = {
-            degree: "",
-            courses: "",
-            stream: "",
-            supplement: "",
-            full: ""
-        }
-        for (ii = 0; ii < b_to.length; ii++) {
-            var reg = RegExp(/Master/)
-            var part1, part2, part3;
+    //         if (b_to[ii].supplement == "") {
+    //             part3 = "";
+    //         } else {
+    //             part3 = "-" + b_to[ii].supplement
+    //         }
 
-            if (b_to[ii].degree.match(reg)) {
-                part1 = b_to[ii].degree.replace("Master of ", "M-");
-            } else if (b_to[ii].degree == "") {
-                part1 = ""
-            } else {
-                part1 = b_to[ii].degree.replace("Bachelor of ", "B-");
-            }
-            if (b_to[ii].stream == "") {
-                part2 = "";
-            } else {
-                part2 = "-" + b_to[ii].stream;
-            }
-            if (b_to[ii].supplement == "") {
-                part3 = "";
-            } else {
-                part3 = "-" + b_to[ii].supplement
-            }
+    //         bb = part1 + part2 + part3;
+    //         info = b_to[ii];
+    //         info.full = bb;
+    //         bt.push(info)
+    //     }
+    //     list[i].belongs_to = bt;
+    // }
 
-            bb = part1 + part2 + part3;
-            info = b_to[ii];
-            info.full = bb;
-            bt.push(info)
-        }
-        list[i].belongs_to = bt;
-    }
+    // list = setData1(list)
+    // res.render("../views/homepage-course-overview.ejs", { course_list: list });
+}
 
-    list = setData1(list)
-    res.render("../views/homepage-course-overview.ejs", { course_list: list });
+function fetchDegreesByCourse(course) {
+    return (
+        new Promise((resolve, reject) => {
+            const query = 'SELECT degree,stream,supplement FROM `degree_course` WHERE `courses` = ?';
+            connection.execute(
+                query, 
+                [course.fullname],
+                (err, result) => (err ? reject(err) : resolve(result))
+            );
+        })
+        .then(degrees => {
+            course.belongs_to = [];
+            
+            degrees.forEach((item, index) => {
+                let {degree, stream, supplement} = item;
+                
+                degree = degree.trim() ? degree.replace("Master of ", "Ms-").replace("Bachelor of ", "Bc-") : "";
+                stream = stream.trim() ? ("-" + stream) : "";
+                supplement = supplement.trim() ? ("-" + supplement) : "";
+                // to be edited
+                course.belongs_to[index] = {};
+                course.belongs_to[index].full = degree + stream + supplement;
+            })
+            return course;
+        })
+    )   
 }
 
 function setData1(data11){
